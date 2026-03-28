54a6c2e07a9c6693f5a1839597893eabb6674541
Bhavesh Gunwant <gunwantbhavesh100@gmail.com>
2026-03-28 14:37:58 +0530

    login api done

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -42,4 +42,42 @@ const registerUser = async (req, res) => {
   }
 };
 
-export default {registerUser};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    const token = generateToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      // secure: true
+      maxAge: 15 * 60 * 1000
+    });
+
+    res.status(200).json({
+      message: "Login successful",
+      user
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export default {registerUser, loginUser};
