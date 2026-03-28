5d6ce05061830b67e26f967fc1b89a0f17d9b5e3
Bhavesh Gunwant <gunwantbhavesh100@gmail.com>
2026-03-28 16:15:58 +0530

    added delete task api

--- Backend/src/controllers/task.controllers.js
+++ Backend/src/controllers/task.controllers.js
@@ -50,10 +50,7 @@ const getTaskById = async (req, res) => {
       });
     }
 
-    if (
-      req.user.role !== "admin" &&
-      task.user.toString() !== req.user._id.toString()
-    ) {
+    if (req.user.role !== "admin" && task.user.toString() !== req.user._id.toString()) {
       return res.status(403).json({
         message: "Not allowed"
       });
@@ -76,10 +73,7 @@ const updateTask = async (req, res) => {
       });
     }
 
-    if (
-      req.user.role !== "admin" &&
-      task.user.toString() !== req.user._id.toString()
-    ) {
+    if (req.user.role !== "admin" && task.user.toString() !== req.user._id.toString()) {
       return res.status(403).json({
         message: "Not allowed"
       });
@@ -101,5 +95,32 @@ const updateTask = async (req, res) => {
   }
 };
 
+const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
 
-export default {createTask,getTasks,getTaskById,updateTask};
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found"
+      });
+    }
+
+    if (req.user.role !== "admin" && task.user.toString() !== req.user._id.toString() ) {
+      return res.status(403).json({
+        message: "Not allowed"
+      });
+    }
+
+    await Task.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      message: "Task deleted"
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+export default {createTask,getTasks,getTaskById,updateTask,deleteTask};
--- Backend/src/routes/task.routes.js
+++ Backend/src/routes/task.routes.js
@@ -9,6 +9,7 @@ router.post("/", protect, taskController.createTask);
 router.get("/", protect, taskController.getTasks);
 router.get("/:id", protect, taskController.getTaskById);
 router.put("/:id", protect, taskController.updateTask);
+router.delete("/:id", protect, taskController.deleteTask);
 
 
 
