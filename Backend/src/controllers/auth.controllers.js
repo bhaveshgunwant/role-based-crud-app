0f61f763bfeffdaff83019fb8c52643996534f62
Bhavesh Gunwant <gunwantbhavesh100@gmail.com>
2026-03-28 14:39:39 +0530

    logout api done

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -80,4 +80,15 @@ const loginUser = async (req, res) => {
   }
 };
 
-export default {registerUser, loginUser};
+export const logoutUser = (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0)
+  });
+
+  res.status(200).json({
+    message: "Logged out successfully"
+  });
+};
+
+export default {registerUser, loginUser, logoutUser};
